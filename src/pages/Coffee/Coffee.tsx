a90aa57881821a0b8cfa9206392a232547d0f4d2
ecocle <146603117+ecocle@users.noreply.github.com>
2023-12-01 09:23:48 +0800

    updated api

--- src/pages/Coffee/Coffee.tsx
+++ src/pages/Coffee/Coffee.tsx
@@ -40,7 +40,7 @@ const Coffee = () => {
 
     useEffect(() => {
         setOpen(true);
-        fetch("https://hualangcafe.com/api/dataCoffee")
+        fetch("/api/dataCoffee")
             .then((response) => response.json())
             .then((data: { Name: string; Price: number }[]) => {
                 const formattedData: CoffeeItem[] = data.map((item) => ({
